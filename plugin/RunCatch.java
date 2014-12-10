a32a56c25a22a2cd59720686b8681079da043e29
VincentK2000 <vincent.kuhlmann@hotmail.com>
2014-12-10 19:00:33 +0100

    Kleine veranderingen ter test

--- plugin/RunCatch.java
+++ plugin/RunCatch.java
@@ -3,7 +3,7 @@ package KartoffelKanaalPlugin.plugin;
 public class RunCatch {
 
 	public static void main(String[] args) {
-		System.out.println("Dit is een Minecraft serverplugin die gerunt zou moeten worden door Bukkit of misschien een ander platform");
+		System.out.println("Dit is een Bukkit serverplugin die gerund zou moeten worden door Bukkit (of misschien een ander platform)");
 	}
 
 	public static void checkNewerVersion() throws Exception{
