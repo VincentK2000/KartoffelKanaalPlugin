2bf24f57add7c1b281ef83335e7ee31065ea0a63
VincentK2000 <vincent.kuhlmann@hotmail.com>
2015-01-10 12:25:44 +0100

    Commit verstuurd vanaf IntelliJ IDEA

---
+++ plugin/kartoffelsystems/BuildTools/BuilderHelmet.java
@@ -0,0 +1,7 @@
+package KartoffelKanaalPlugin.plugin.kartoffelsystems.BuildTools;
+
+/**
+ * Created by Vincent on 10-Jan-15.
+ */
+public class BuilderHelmet {
+}
---
+++ plugin/kartoffelsystems/BuildTools/CreaBuilderHelmet.java
@@ -0,0 +1,8 @@
+package KartoffelKanaalPlugin.plugin.kartoffelsystems.BuildTools;
+
+/**
+ * Created by Vincent on 10-Jan-15.
+ */
+public class CreaBuilderHelmet extends BuilderHelmet {
+
+}
