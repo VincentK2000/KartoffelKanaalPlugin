29ec2fe84a7a529a934073bc9e44b973b78fe606
VincentK2000 <vincent.kuhlmann@hotmail.com>
2015-01-10 12:32:04 +0100

    Setup of BuildTools service

---
+++ plugin/kartoffelsystems/BuildTools/BuildToolsService.java
@@ -0,0 +1,23 @@
+package KartoffelKanaalPlugin.plugin.kartoffelsystems.BuildTools;
+
+import KartoffelKanaalPlugin.plugin.kartoffelsystems.KartoffelService;
+
+/**
+ * Created by Vincent on 10-Jan-15.
+ */
+public class BuildToolsService extends KartoffelService {
+    public BuildToolsService(){
+        super("BuildTools");
+    }
+
+
+    @Override
+    protected void _enableCore() throws Exception {
+
+    }
+
+    @Override
+    protected void _disableCore() throws Exception {
+
+    }
+}
--- plugin/kartoffelsystems/BuildTools/BuilderHelmet.java
+++ plugin/kartoffelsystems/BuildTools/BuilderHelmet.java
@@ -4,4 +4,5 @@ package KartoffelKanaalPlugin.plugin.kartoffelsystems.BuildTools;
  * Created by Vincent on 10-Jan-15.
  */
 public class BuilderHelmet {
+    
 }
